6e11f31533e7a4250a8f5ba451b2bb166df77d40
BasilYes <basilyevs@gmail.com>
2024-08-02 13:44:17 +0300

    relative path

--- plugin/export_scripts_template/export_plugin.gd
+++ plugin/export_scripts_template/export_plugin.gd
@@ -24,8 +24,7 @@ func _enter_tree():
 	# Initialization of the plugin goes here.
 	export_plugin = AndroidExportPlugin.new()
 	add_export_plugin(export_plugin)
-	add_autoload_singleton("FirebaseAnalytics",
-			"res://addons/FirebaseAnalyticsGodotPlugin/plugin.gd")
+	add_autoload_singleton("FirebaseAnalytics", "plugin.gd")
 
 
 func _exit_tree():
